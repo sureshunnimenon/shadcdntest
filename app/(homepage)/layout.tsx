32bf9176ef1ee39ec9c313d40534a6a05c503c96
sureshunnimenon <suresh.unni.menon@gmail.com>
2023-08-22 20:19:16 +0530

    small cosmetic change in dashboard layout sidebar component

--- app/(homepage)/layout.tsx
+++ app/(homepage)/layout.tsx
@@ -14,26 +14,29 @@ function Homepage({ children }: { children: React.ReactNode }) {
     <div className="h-full relative">
       <div className="hidden h-full md:flex md:flex-col md:fixed md:w-[224px] md:inset-y-0 bg-saffron z-[80]">
         <div className="relative flex align-middle justify-between h-3 w-3 md:h-4 md:w-4 lg:h-10 lg:w-10 mr-6 mb-16">
-          <div><CldImage className="ml-3 mt-3" fill alt="OM logo" src="om/ocm28xxc0mrkdqmvzvmd" /></div>
-          <h1
-          className={cn("text-[8px] lg:text-[12px] font-bold text-stone-800 ml-16 mt-3", font.className)}
+          <div><CldImage fill alt="OM logo" src="om/ocm28xxc0mrkdqmvzvmd" /></div>
+          {/* <h1
+          className={cn("text-[8px] lg:text-[12px] font-bold text-stone-800 pl-12", font.className)}
         >
           OM- ExBeyond
-        </h1>
+        </h1> */}
+        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white ml-12">
+                  OM-ExBeyond
+                </span>
         
         </div>
         
         {menulinks?.map((menuitem, index) => {
           return (
             <div key={index}>
-              <ul className="flex px-6 py-3">
+              <ul className="flex px-3 py-3 font-bold uppercase">
                 <CldImage
                   width="40"
                   height="50"
                   alt="menu icon"
                   src={menuitem.icon}
                 />
-                <li className="text-[12px] pl-1 text-center text-zinc-800">
+                <li className="text-[12px] pl-1 text-center text-white">
                   {menuitem.name}
                 </li>
               </ul>
